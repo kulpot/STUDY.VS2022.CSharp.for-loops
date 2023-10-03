d0cadd31705bd42d0e14a744fe1f1c58f14d448a
Kulpot <kulpotwahu@gmail.com>
2023-10-03 13:13:17 +0800

    Add project files.

---
+++ for loops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=GRs1slN67lw&list=PLF932FF551E0ADD16&index=6
+//
+
+class MyClass
+{
+    static void Main()
+    {
+        int i = 1;
+        while (i <= 20)
+        {
+            Console.WriteLine(i);
+            i = i + 1;
+        }
+        Console.WriteLine("Done"); // output 1 to 20 Done
+    }
+}
